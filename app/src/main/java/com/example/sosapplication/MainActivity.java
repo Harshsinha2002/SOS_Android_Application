d46c074ddff61fbd748597532e4cb7065463a412
Harshsinha2002 <harshsinha2002@gmail.com>
2024-02-28 02:17:07 +0530

    SOS Women Safety Application

--- app/src/main/java/com/example/sosapplication/MainActivity.java
+++ app/src/main/java/com/example/sosapplication/MainActivity.java
@@ -45,11 +45,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.annotations.concurrent.Background;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
     TextView User_Address;
+    TextView SOS_Contacts_txt;
     private static final int REQUEST_CHECK_SETTING = 1001;
 
     private  static  final  int Call_Permission_Code = 1000;
@@ -59,6 +64,19 @@ public class MainActivity extends AppCompatActivity {
 
     Dialog dialog;
     Button Dialog_Cancelbtn, Dialog_Logoutbtn;
+    DatabaseReference db;
+
+    String email;
+
+    String Sos_Contact_1;
+    String Sos_Contact_2;
+    String Sos_Contact_3;
+    FirebaseUser user;
+
+    String User_id, User_FullName, User_PhoneNo, User_Address_Line1, User_Address_Line2, User_PinCode;
+    String Sos_Contact_1_FullName, Sos_Contact_1_PhoneNo, Sos_Contact_2_FullName, Sos_Contact_2_PhoneNo, Sos_Contact_3_FullName, Sos_Contact_3_PhoneNo;
+    String Quick_Contact_1_FullName, Quick_Contact_1_PhoneNo,Quick_Contact_2_FullName, Quick_Contact_2_PhoneNo, Quick_Contact_3_FullName, Quick_Contact_3_PhoneNo, Quick_Contact_4_FullName, Quick_Contact_4_PhoneNo, Quick_Contact_5_FullName, Quick_Contact_5_PhoneNo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -66,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        User_Address = findViewById(R.id.User_location);
         Button  SOS_Button = findViewById(R.id.SOS_btn);
         locationRequest = LocationRequest.create(); // create a location request
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); // set how accurately the location will be provided
@@ -77,9 +94,77 @@ public class MainActivity extends AppCompatActivity {
         ImageView Call_Women_Helpline = findViewById(R.id.Call_Women_Helpline);
         ImageView Call_Ambulance = findViewById(R.id.Call_Ambulance);
         ImageView SafePlaces = findViewById(R.id.SafePlaces);
+        ImageView User_Profile = findViewById(R.id.User_Profile);
+        ImageView Quick_Contacts = findViewById(R.id.Quick_Contacts);
+        ImageView Safety_Products = findViewById(R.id.Safety_Products);
+        ImageView About_Us = findViewById(R.id.About_Us);
+        ImageView Self_Defence_Tips = findViewById(R.id.Self_Defence_Tips);
+        SOS_Contacts_txt = findViewById(R.id.SOS_Contacts_txt);
 
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.pink)));
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null)
+        {
+            email = user.getEmail();
+            db = FirebaseDatabase.getInstance().getReference("User Info");
+            db.child(email.replace(".", "")).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        if(task.getResult().exists())
+                        {
+                            Toast.makeText(MainActivity.this, "Welcome Back", Toast.LENGTH_SHORT).show();
+                        }
+
+                        else
+                        {
+                            startActivity(new Intent(MainActivity.this, Create_New_User_Info_Page.class));
+                            finish();
+                        }
+                    }
+                }
+            });
+        }
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null)
+        {
+            email = user.getEmail();
+        }
+        db = FirebaseDatabase.getInstance().getReference("User Info");
+        db.child(email.replace(".","")).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task)
+            {
+                if(task.isSuccessful())
+                {
+                    if(task.getResult().exists())
+                    {
+                        DataSnapshot dataSnapshot = task.getResult();
+                        String Sos_Contact_1_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_1_PhoneNo").getValue());
+                        String Sos_Contact_2_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_2_PhoneNo").getValue());
+                        String Sos_Contact_3_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_3_PhoneNo").getValue());
+
+                        Sos_Contact_1 = Sos_Contact_1_PhoneNo;
+                        Sos_Contact_2 = Sos_Contact_2_PhoneNo;
+                        Sos_Contact_3 = Sos_Contact_3_PhoneNo;
+
+                        SOS_Contacts_txt.setText(Sos_Contact_1_PhoneNo + ", " + Sos_Contact_2_PhoneNo + ", " + Sos_Contact_3_PhoneNo);
+                    }
+
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Set User Information!!", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
 
 
         SOS_Button.setOnClickListener(new View.OnClickListener() {
@@ -107,7 +192,6 @@ public class MainActivity extends AppCompatActivity {
                                         Latitude = (double)latitude;
                                         Longitude = (double) longitude;
 
-                                        User_Address.setText("Latitude : " + Latitude  + "\nLongitude : " + Longitude );
                                         sendSMS(Longitude,Latitude);
 
                                         Toast.makeText(MainActivity.this, "SOS Message sent!!",Toast.LENGTH_SHORT).show();
@@ -187,7 +271,182 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(MainActivity.this, SafePlaces_Page.class);
+                Intent intent = new Intent(MainActivity.this,SafetyPlaces_Page.class);
+                startActivity(intent);
+            }
+        });
+
+        User_Profile.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null)
+                {
+                    String email = user.getEmail();
+                    User_id = email;
+                }
+
+                db = FirebaseDatabase.getInstance().getReference("User Info");
+                db.child(User_id.replace(".", "")).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            if(task.getResult().exists())
+                            {
+                                DataSnapshot dataSnapshot = task.getResult();
+
+                                User_id = String.valueOf(dataSnapshot.child("User_id").getValue());
+                                User_FullName = String.valueOf(dataSnapshot.child("User_FullName").getValue());
+                                User_PhoneNo = String.valueOf(dataSnapshot.child("User_PhoneNo").getValue());
+                                User_Address_Line1 = String.valueOf(dataSnapshot.child("User_Address_Line1").getValue());
+                                User_Address_Line2 = String.valueOf(dataSnapshot.child("User_Address_Line2").getValue());
+                                User_PinCode = String.valueOf(dataSnapshot.child("User_PinCode").getValue());
+
+
+                                Sos_Contact_1_FullName = String.valueOf(dataSnapshot.child("Sos_Contact_1_FullName").getValue());
+                                Sos_Contact_1_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_1_PhoneNo").getValue());
+                                Sos_Contact_2_FullName = String.valueOf(dataSnapshot.child("Sos_Contact_2_FullName").getValue());
+                                Sos_Contact_2_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_2_PhoneNo").getValue());
+                                Sos_Contact_3_FullName = String.valueOf(dataSnapshot.child("Sos_Contact_3_FullName").getValue());
+                                Sos_Contact_3_PhoneNo = String.valueOf(dataSnapshot.child("Sos_Contact_3_PhoneNo").getValue());
+
+
+                                Quick_Contact_1_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_1_FullName").getValue());
+                                Quick_Contact_1_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_1_PhoneNo").getValue());
+                                Quick_Contact_2_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_2_FullName").getValue());
+                                Quick_Contact_2_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_2_PhoneNo").getValue());
+                                Quick_Contact_3_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_3_FullName").getValue());
+                                Quick_Contact_3_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_3_PhoneNo").getValue());
+                                Quick_Contact_4_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_4_FullName").getValue());
+                                Quick_Contact_4_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_4_PhoneNo").getValue());
+                                Quick_Contact_5_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_5_FullName").getValue());
+                                Quick_Contact_5_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_5_PhoneNo").getValue());
+
+
+                                Intent intent = new Intent(MainActivity.this, User_Profile_Page.class);
+
+                                intent.putExtra("User_id", User_id);
+                                intent.putExtra("User_FullName", User_FullName);
+                                intent.putExtra("User_PhoneNo", User_PhoneNo);
+                                intent.putExtra("User_Address_Line1", User_Address_Line1);
+                                intent.putExtra("User_Address_Line2", User_Address_Line2);
+
+
+                                intent.putExtra("Sos_Contact_1_FullName", Sos_Contact_1_FullName);
+                                intent.putExtra("Sos_Contact_1_PhoneNo", Sos_Contact_1_PhoneNo);
+                                intent.putExtra("Sos_Contact_2_FullName", Sos_Contact_2_FullName);
+                                intent.putExtra("Sos_Contact_2_PhoneNo", Sos_Contact_2_PhoneNo);
+                                intent.putExtra("Sos_Contact_3_FullName", Sos_Contact_3_FullName);
+                                intent.putExtra("Sos_Contact_3_PhoneNo", Sos_Contact_3_PhoneNo);
+
+
+                                intent.putExtra("Quick_Contact_1_FullName", Quick_Contact_1_FullName);
+                                intent.putExtra("Quick_Contact_1_PhoneNo", Quick_Contact_1_PhoneNo);
+                                intent.putExtra("Quick_Contact_2_FullName", Quick_Contact_2_FullName);
+                                intent.putExtra("Quick_Contact_2_PhoneNo", Quick_Contact_2_PhoneNo);
+                                intent.putExtra("Quick_Contact_3_FullName", Quick_Contact_3_FullName);
+                                intent.putExtra("Quick_Contact_3_PhoneNo", Quick_Contact_3_PhoneNo);
+                                intent.putExtra("Quick_Contact_4_FullName", Quick_Contact_4_FullName);
+                                intent.putExtra("Quick_Contact_4_PhoneNo", Quick_Contact_4_PhoneNo);
+                                intent.putExtra("Quick_Contact_5_FullName", Quick_Contact_5_FullName);
+                                intent.putExtra("Quick_Contact_5_PhoneNo", Quick_Contact_5_PhoneNo);
+
+                                startActivity(intent);
+                            }
+                        }
+                    }
+                });
+
+            }
+        });
+
+        Quick_Contacts.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null)
+                {
+                    String email = user.getEmail();
+                    User_id = email;
+                }
+
+                db = FirebaseDatabase.getInstance().getReference("User Info");
+                db.child(User_id.replace(".", "")).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<DataSnapshot> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            if(task.getResult().exists())
+                            {
+                                DataSnapshot dataSnapshot = task.getResult();
+                                Quick_Contact_1_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_1_FullName").getValue());
+                                Quick_Contact_1_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_1_PhoneNo").getValue());
+                                Quick_Contact_2_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_2_FullName").getValue());
+                                Quick_Contact_2_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_2_PhoneNo").getValue());
+                                Quick_Contact_3_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_3_FullName").getValue());
+                                Quick_Contact_3_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_3_PhoneNo").getValue());
+                                Quick_Contact_4_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_4_FullName").getValue());
+                                Quick_Contact_4_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_4_PhoneNo").getValue());
+                                Quick_Contact_5_FullName = String.valueOf(dataSnapshot.child("Quick_Contact_5_FullName").getValue());
+                                Quick_Contact_5_PhoneNo =  String.valueOf(dataSnapshot.child("Quick_Contact_5_PhoneNo").getValue());
+
+
+                                Intent intent = new Intent(MainActivity.this, Quick_Contacts_Page.class);
+
+                                intent.putExtra("Quick_Contact_1_FullName", Quick_Contact_1_FullName);
+                                intent.putExtra("Quick_Contact_1_PhoneNo", Quick_Contact_1_PhoneNo);
+                                intent.putExtra("Quick_Contact_2_FullName", Quick_Contact_2_FullName);
+                                intent.putExtra("Quick_Contact_2_PhoneNo", Quick_Contact_2_PhoneNo);
+                                intent.putExtra("Quick_Contact_3_FullName", Quick_Contact_3_FullName);
+                                intent.putExtra("Quick_Contact_3_PhoneNo", Quick_Contact_3_PhoneNo);
+                                intent.putExtra("Quick_Contact_4_FullName", Quick_Contact_4_FullName);
+                                intent.putExtra("Quick_Contact_4_PhoneNo", Quick_Contact_4_PhoneNo);
+                                intent.putExtra("Quick_Contact_5_FullName", Quick_Contact_5_FullName);
+                                intent.putExtra("Quick_Contact_5_PhoneNo", Quick_Contact_5_PhoneNo);
+
+                                startActivity(intent);
+                            }
+                        }
+                    }
+                });
+            }
+        });
+
+        Safety_Products.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, Safety_Products_Page.class);
+                startActivity(intent);
+            }
+        });
+
+        About_Us.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, About_Us_Page.class);
+                startActivity(intent);
+            }
+        });
+
+        Self_Defence_Tips.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, Self_Defence_Tips_Page.class);
                 startActivity(intent);
             }
         });
